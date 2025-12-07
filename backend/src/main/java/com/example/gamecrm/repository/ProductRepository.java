a368c0ff3d375bd768c6706303e82f25d479dd1e
Aldotetraosa <77131522+AldixKun@users.noreply.github.com>
2025-12-07 17:40:07 +0100

    Delete backend directory - antiguo

--- backend/src/main/java/com/example/gamecrm/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gamecrm.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.gamecrm.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product,String> {
-
-}
